0ea23fd05edb4f9c562a3a40ba03901131081e93
PBarkema <pieterwbarkema@gmail.com>
2022-12-12 16:35:29 +0100

    n jobs changed

---
+++ server/extract_aomic_sites_fmri.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+x=pd.read_csv("/project_cephfs/3022017.02/projects/hansav/Run4/data/control_metadata.csv")
+
+AOMIC= x.loc[x['dataset']=="AOMIC_PIOP2"]
+print(AOMIC.head(n=5))
+
+AOMIC.to_csv("/project_cephfs/3022051.01/docs/fmri_data/AOMIC_covs")
