b6eca2fd4de9aad917e8fdbf14b02e0b8f55bf6d
PBarkema <39303377+PBarkema@users.noreply.github.com>
2023-02-01 15:27:51 +0100

    Update unpickler.py

--- server/unpickler.py
+++ server/unpickler.py
@@ -1,8 +1,7 @@
 import pandas as pd
-path= r"C:\Users\piebar\Downloads\Z_transfer.pkl"
-x=pd.read_pickle(path)
-print(x.head)
-
+path=r"/project_cephfs/3022051.01/sessions/test_session_99ec5c53c62f48f7894b9b585cbaad2b/Z_transfer.pkl"
+x = pd.read_pickle(path)
+#print(x.0)
 import matplotlib.pyplot as plt
 for i in range(10):
     plt.clf()
