0b5e4e05d0975d5f2c8d87ac5a3c32fb368ccc51
PBarkema <pieterwbarkema@gmail.com>
2023-02-23 15:17:54 +0100

    ensure uptodate

--- server/unpickler.py
+++ server/unpickler.py
@@ -1,11 +1,12 @@
 import pandas as pd
 path=r""
-x = pd.read_pickle(path)
-#print(x.0)
-import matplotlib.pyplot as plt
-for i in range(10):
-    plt.clf()
-    t= x.iloc[:, i]
-    fig, ax = plt.subplots()
-    ax.hist(t)
-    plt.show()
+x = pd.read_pickle("/project_cephfs/3022051.01/sessions/3839801196b2401f83576cce02fa2414/sitenum_ad.pkl")
+y = x.to_numpy(dtype="int")
+print(x)
+# import matplotlib.pyplot as plt
+# for i in range(10):
+#     plt.clf()
+#     t= x.iloc[:, i]
+#     fig, ax = plt.subplots()
+#     ax.hist(t)
+#     plt.show()
