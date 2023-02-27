55d2aa87729f4e3345b14d6007c836904a9d94f3
PBarkema <39303377+PBarkema@users.noreply.github.com>
2023-02-27 18:16:41 +0100

    Update entrypoint.sh

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -2,13 +2,13 @@
 
 mkdir -p ~/.ssh
 
-if [ ! -f ~/.ssh/id_rsa ]; then
-  # Generate a new SSH key, that the user should copy to the submission host of qsub.
-  ssh-keygen -t rsa -N "" -C "PCNportal" -f ~/.ssh/id_rsa
-  echo "copy the following content to ~/.ssh/authorized_keys"
-  cat ~/.ssh/id_rsa.pub
-fi
-env > /tmp/env.txt
+# Disabled for review testing.
+#if [ ! -f ~/.ssh/id_rsa ]; then
+# Generate a new SSH key, that the user should copy to the submission host of qsub.
+#  ssh-keygen -t rsa -N "" -C "PCNportal" -f ~/.ssh/id_rsa
+#  echo "copy the following content to ~/.ssh/authorized_keys"
+#  cat ~/.ssh/id_rsa.pub
+#fi
 
 chmod -R 600 /root/
 gunicorn --timeout=1000 --workers=5 --threads=1 -b 0.0.0.0:80 app:server
